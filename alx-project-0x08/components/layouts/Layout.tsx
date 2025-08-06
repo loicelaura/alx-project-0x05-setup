88856622572b09db61d5b82778918afd59ef8d9e
loice okello <loicelaura@gmail.com>
2025-08-06 16:17:12 +0300

    added alx-project 08

---
+++ alx-project-0x08/components/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactComponentProps } from "@/interfaces";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Layout;
